22bc6a61eb2a55844cc793d7892def77d3b418b2
berrutig <38107827+berrutig@users.noreply.github.com>
2018-10-02 20:51:20 +0200

    Update setup_sonar.sh

--- Infrastructure/bin/setup_sonar.sh
+++ Infrastructure/bin/setup_sonar.sh
@@ -14,55 +14,10 @@ echo "Setting up Sonarqube in project $GUID-sonarqube"
 # Ideally just calls a template
 # oc new-app -f ../templates/sonarqube.yaml --param .....
 
-
-
 #oc new-project $GUID-sonarqube --display-name "Shared Sonarqube"
 #oc policy add-role-to-user admin ${USER} -n ${GUID}-sonarqube
 
 
 oc project $GUID-sonarqube 
-
-#####     Create a new PostgreSQL database
-
-
-#oc annotate namespace ${GUID}-sonarqube openshift.io/requester=${USER} --overwrite
-docker pull registry.access.redhat.com/openshift3/mongodb-24-rhel7
-#oc new-app --template=postgresql-persistent --param POSTGRESQL_USER=sonar --param POSTGRESQL_PASSWORD=sonar --param POSTGRESQL_DATABASE=sonar --param VOLUME_CAPACITY=4Gi --labels=app=sonarqube_db
-oc new-app --template=postgresql-persistent --param POSTGRESQL_USER=sonar --param POSTGRESQL_PASSWORD=sonar --param POSTGRESQL_DATABASE=sonar --param VOLUME_CAPACITY=4Gi --labels=app=sonarqube_db -n $GUID-sonarqube
-
-
-sleep 100
-
-
-#####    Create a new SonarQube instance from docker.io/wkulhanek/sonarqube:6.7.4####    
-
-
-
-oc new-app --docker-image=wkulhanek/sonarqube:6.7.4 --env=SONARQUBE_JDBC_USERNAME=sonar --env=SONARQUBE_JDBC_PASSWORD=sonar --env=SONARQUBE_JDBC_URL=jdbc:postgresql://postgresql/sonar --labels=app=sonarqube
-
-oc rollout pause dc sonarqube
-
-
-#####    Configure SonarQube appropriately for resources, deployment strategy, persistent volumes, and readiness and liveness probes
-
-oc expose service sonarqube
-
-echo "apiVersion: v1
-kind: PersistentVolumeClaim
-metadata:
-  name: sonarqube-pvc
-spec:
-  accessModes:
-  - ReadWriteOnce
-  resources:
-    requests:
-      storage: 4Gi" | oc create -f -
-oc set volume dc/sonarqube --add --overwrite --name=sonarqube-volume-1 --mount-path=/opt/sonarqube/data/ --type persistentVolumeClaim --claim-name=sonarqube-pvc
-
-oc set resources dc/sonarqube --limits=memory=3Gi,cpu=2 --requests=memory=2Gi,cpu=1
-oc patch dc sonarqube --patch='{ "spec": { "strategy": { "type": "Recreate" }}}'
-
-oc set probe dc/sonarqube --liveness --failure-threshold 3 --initial-delay-seconds 40 -- echo ok
-oc set probe dc/sonarqube --readiness --failure-threshold 3 --initial-delay-seconds 20 --get-url=http://:9000/about
-
-oc rollout resume dc sonarqube
+oc new-app -f ./Infrastructure/templates/sonarqube.yml --param=SONARQUBE_IMAGE=docker.io/wkulhanek/sonarqube \
+  --param=SONARQUBE_VERSION=7.3 -n "${GUID}-sonarqube"
