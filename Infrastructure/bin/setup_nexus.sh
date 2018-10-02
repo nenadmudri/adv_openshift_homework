469daaf33aad5919b750efc57b641ab3f5ab1841
berrutig <38107827+berrutig@users.noreply.github.com>
2018-10-02 17:24:06 +0200

    Update setup_nexus.sh

--- Infrastructure/bin/setup_nexus.sh
+++ Infrastructure/bin/setup_nexus.sh
@@ -44,31 +44,31 @@ oc new-app sonatype/nexus3:latest
 oc expose svc nexus3
 oc rollout pause dc nexus3
 echo 'Pause nexus deployment'
-#oc new-app -f ./Infrastructure/templates/nexus.yml -n "${GUID}-nexus"
+oc new-app -f ./Infrastructure/templates/nexus.yml -n "${GUID}-nexus"
 
 
 
 #####    Configure Nexus appropriately for resources, deployment strategy, persistent volumes, and readiness and liveness probes
 
-oc patch dc nexus3 --patch='{ "spec": { "strategy": { "type": "Recreate" }}}'
-oc set resources dc nexus3 --limits=memory=2Gi --requests=memory=1Gi
+#oc patch dc nexus3 --patch='{ "spec": { "strategy": { "type": "Recreate" }}}'
+#oc set resources dc nexus3 --limits=memory=2Gi --requests=memory=1Gi
 
-echo "apiVersion: v1
-kind: PersistentVolumeClaim
-metadata:
-  name: nexus-pvc
-spec:
-  accessModes:
-  - ReadWriteOnce
-  resources:
-    requests:
-      storage: 4Gi" | oc create -f -
+#echo "apiVersion: v1
+#kind: PersistentVolumeClaim
+#metadata:
+#  name: nexus-pvc
+#spec:
+#  accessModes:
+#  - ReadWriteOnce
+#  resources:
+#    requests:
+#      storage: 4Gi" | oc create -f -
 
-oc set volume dc/nexus3 --add --overwrite --name=nexus3-volume-1 --mount-path=/nexus-data/ --type persistentVolumeClaim --claim-name=nexus-pvc
-oc set probe dc/nexus3 --liveness --failure-threshold 3 --initial-delay-seconds 60 -- echo ok
-oc set probe dc/nexus3 --readiness --failure-threshold 3 --initial-delay-seconds 60 --get-url=http://:8081/repository/maven-public/
+#oc set volume dc/nexus3 --add --overwrite --name=nexus3-volume-1 --mount-path=/nexus-data/ --type persistentVolumeClaim --claim-name=nexus-pvc
+#oc set probe dc/nexus3 --liveness --failure-threshold 3 --initial-delay-seconds 60 -- echo ok
+#oc set probe dc/nexus3 --readiness --failure-threshold 3 --initial-delay-seconds 60 --get-url=http://:8081/repository/maven-public/
 
-oc rollout resume dc nexus3
+#oc rollout resume dc nexus3
 
 #sleep 350
 while : ; do
