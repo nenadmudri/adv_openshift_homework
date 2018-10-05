73d76a30cf8e7fe5ca1e5aa5a9231a2c51ddafa7
berrutig <38107827+berrutig@users.noreply.github.com>
2018-10-06 01:38:23 +0200

    Update setup_prod.sh

--- Infrastructure/bin/setup_prod.sh
+++ Infrastructure/bin/setup_prod.sh
@@ -25,9 +25,10 @@ echo "Setting up Parks Production Environment in project ${GUID}-parks-prod"
 
 #oc new-project $GUID-parks-prod --display-name "Shared Parks Prod"
 oc project $GUID-parks-prod 
-oc policy add-role-to-user admin ${USER} -n ${GUID}-parks-prod
-oc policy add-role-to-group system:image-puller system:serviceaccounts:${GUID}-parks-prod -n ${GUID}-parks-dev
-oc policy add-role-to-user edit system:serviceaccount:${GUID}-jenkins:jenkins -n ${GUID}-parks-prod
+oc policy add-role-to-user view --serviceaccount=default -n $GUID-parks-prod
+oc policy add-role-to-group system:image-puller system:serviceaccounts:$GUID-parks-prod -n $GUID-parks-dev
+oc policy add-role-to-user edit system:serviceaccount:$GUID-jenkins:jenkins -n $GUID-parks-prod
+oc policy add-role-to-user admin system:serviceaccount:gpte-jenkins:jenkins -n $GUID-parks-prod
 
 
 
