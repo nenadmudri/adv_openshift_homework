4e7490d0ed11ad800c0577060c8215fe4cebb2b0
berrutig <38107827+berrutig@users.noreply.github.com>
2018-10-06 10:00:34 +0200

    Update setup_dev.sh

--- Infrastructure/bin/setup_dev.sh
+++ Infrastructure/bin/setup_dev.sh
@@ -180,9 +180,9 @@ oc set env dc/parksmap --from=configmap/parksmap-config
 
 echo 'oc patch dc and deployment hooks'
 
-#oc patch dc/parksmap --patch "spec: { strategy: {type: Rolling, rollingParams: {post: {failurePolicy: Ignore, execNewPod: {containerName: parksmap, command: ['curl -XGET http://localhost:8080/ws/data/load/']}}, timeoutSeconds: 6000}}}"
-#oc patch dc/mlbparks --patch "spec: { strategy: {type: Rolling, rollingParams: {post: {failurePolicy: Ignore, execNewPod: {containerName: mlbparks, command: ['curl -XGET http://localhost:8080/ws/data/load/']}}, timeoutSeconds: 6000}}}"
-#oc patch dc/nationalparks --patch "spec: { strategy: {type: Rolling, rollingParams: {post: {failurePolicy: Ignore, execNewPod: {containerName: nationalparks, command: ['curl -XGET http://localhost:8080/ws/data/load/']}}, timeoutSeconds: 6000}}}"
+oc patch dc/parksmap --patch "spec: { strategy: {type: Rolling, rollingParams: {post: {failurePolicy: Ignore, execNewPod: {containerName: parksmap, command: ['curl -XGET http://localhost:8080/ws/data/load/']}}}, timeoutSeconds: 6000}}"
+oc patch dc/mlbparks --patch "spec: { strategy: {type: Rolling, rollingParams: {post: {failurePolicy: Ignore, execNewPod: {containerName: mlbparks, command: ['curl -XGET http://localhost:8080/ws/data/load/']}}}, timeoutSeconds: 6000}}"
+oc patch dc/nationalparks --patch "spec: { strategy: {type: Rolling, rollingParams: {post: {failurePolicy: Ignore, execNewPod: {containerName: nationalparks, command: ['curl -XGET http://localhost:8080/ws/data/load/']}}}, timeoutSeconds: 6000}}"
 ##### Set deploymenth  hooks
     
 
@@ -203,6 +203,11 @@ oc set deployment-hook dc/parksmap  -n ${GUID}-parks-dev --post -c parksmap --fa
 #oc rollout resume dc/nationalparks  -n $GUID-parks-dev
 #oc rollout resume dc/parksmap  -n $GUID-parks-dev
 #sleep 300
+oc rollout latest dc/mlbparks  -n $GUID-parks-dev
+oc rollout latest dc/nationalparks  -n $GUID-parks-dev
+oc rollout latest dc/parksmap  -n $GUID-parks-dev
+sleep 300
+
 echo '********************************************************'
 echo 'Dev deployment terminated'
 echo '********************************************************'
