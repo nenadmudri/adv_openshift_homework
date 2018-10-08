43b8fe4bb25beee379eb53af95042a45f60bf367
berrutig <38107827+berrutig@users.noreply.github.com>
2018-10-08 21:10:45 +0200

    Update setup_dev.sh

--- Infrastructure/bin/setup_dev.sh
+++ Infrastructure/bin/setup_dev.sh
@@ -178,9 +178,9 @@
 	oc new-app ${GUID}-parks-dev/mlbparks:0.0-0 --name=mlbparks --allow-missing-imagestream-tags=true -n ${GUID}-parks-dev
 	oc new-app ${GUID}-parks-dev/nationalparks:0.0-0 --name=nationalparks --allow-missing-imagestream-tags=true -n ${GUID}-parks-dev
 	oc new-app ${GUID}-parks-dev/parksmap:0.0-0 --name=parksmap --allow-missing-imagestream-tags=true -n ${GUID}-parks-dev
-	#oc rollout pause dc/mlbparks
-	#oc rollout pause dc/nationalparks
-	#oc rollout pause dc/parksmap
+	oc rollout pause dc/mlbparks
+	oc rollout pause dc/nationalparks
+	oc rollout pause dc/parksmap
 	
 
 	
@@ -268,13 +268,13 @@
 	#echo '********************************************************'
 	
 
-	#oc rollout resume dc/mlbparks  -n $GUID-parks-dev
-	#oc rollout resume dc/nationalparks  -n $GUID-parks-dev
-	#oc rollout resume dc/parksmap  -n $GUID-parks-dev
+	oc rollout resume dc/mlbparks  -n $GUID-parks-dev
+	oc rollout resume dc/nationalparks  -n $GUID-parks-dev
+	oc rollout resume dc/parksmap  -n $GUID-parks-dev
 	#sleep 300
-	oc rollout latest dc/mlbparks  -n $GUID-parks-dev
-	oc rollout latest dc/nationalparks  -n $GUID-parks-dev
-	oc rollout latest dc/parksmap  -n $GUID-parks-dev
+	#oc rollout latest dc/mlbparks  -n $GUID-parks-dev
+	#oc rollout latest dc/nationalparks  -n $GUID-parks-dev
+	#oc rollout latest dc/parksmap  -n $GUID-parks-dev
 	#sleep 300
 	
 
