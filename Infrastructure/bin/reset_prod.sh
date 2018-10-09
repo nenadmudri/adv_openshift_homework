03472a966569b7f20bea87d8d1d06f6622303143
berrutig <38107827+berrutig@users.noreply.github.com>
2018-10-09 09:31:05 +0200

    Update reset_prod.sh

--- Infrastructure/bin/reset_prod.sh
+++ Infrastructure/bin/reset_prod.sh
@@ -19,6 +19,6 @@ echo "Resetting Parks Production Environment in project ${GUID}-parks-prod to Gr
 
 ####    Reset the three microservices to the Green version to guarantee a Blue rollout upon the first pipeline run
 
-oc patch route/mlbparks -p '{"spec":{"to":{"name":"g-mlbparks"}}}' -n $GUID-parks-prod 
-oc patch route/nationalparks -p '{"spec":{"to":{"name":"g-nationalparks"}}}' -n $GUID-parks-prod 
-oc patch route/parksmap   -p '{"spec":{"to":{"name":"g-parksmap"}}}' -n $GUID-parks-prod 
+oc patch route/mlbparks -p '{"spec":{"to":{"name":"mlbparks-green"}}}' -n $GUID-parks-prod 
+oc patch route/nationalparks -p '{"spec":{"to":{"name":"nationalparks-green"}}}' -n $GUID-parks-prod 
+oc patch route/parksmap   -p '{"spec":{"to":{"name":"parksmap-green"}}}' -n $GUID-parks-prod 
